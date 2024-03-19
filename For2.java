586822f543cd17d141d6050e59b3e2a61c58a157
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-03-19 20:41:59 -0300

    Add files via upload

---
+++ For2.java
@@ -0,0 +1,14 @@
+package for2;
+
+public class For2 {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 15; i < 201; i++) {
+			int resultado=(int) (Math.pow(i, 2));
+			System.out.println("O quadrado de"+i+": "+resultado);
+			
+		}
+	}
+
+}

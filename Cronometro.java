41df819c620450a12a3e156ed88a22babb638af6
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-03-21 20:15:57 -0300

    Add files via upload

---
+++ Cronometro.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class Cronometro {
+
+	public static void main(String[] args) {
+
+		for (int minutos = 0; minutos < 60; minutos++) {
+			for (int segundos = 0; segundos < 60; segundos++) {
+				System.out.println(minutos + ";" + segundos);
+			}
+		}
+
+	}
+}

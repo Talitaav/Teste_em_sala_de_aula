770fede4b617ecbfdca32f0a1e6a5cc056898c1b
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-03-21 21:17:04 -0300

    Add files via upload

---
+++ Temporizador.java
@@ -0,0 +1,15 @@
+package teste;
+
+public class Temporizador {
+
+	public static void main(String[] args) {
+
+		for (int minutos = 59; minutos >= 0 || minutos ==0; minutos--) {
+			for (int segundos = 59; segundos > 0; segundos--) {
+				System.out.println(minutos + ";" + segundos);
+			}
+		}
+
+	}
+
+}

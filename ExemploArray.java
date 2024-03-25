c5e14c4478529eb05bb05dac3827feddc8303887
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-03-25 20:56:21 -0300

    Update ExemploArray.java

--- ExemploArray.java
+++ ExemploArray.java
@@ -2,7 +2,7 @@ package exemplo;
 
 public class ExemploArray {
 	public static void main(String[] args) {
-		String[] cars = {"Volvo", "Bmw", "Ford", "Mazda"};
+		String[] cars = { "Volvo", "Bmw", "Ford", "Mazda" };
 		System.out.println(cars[0]);
 	}
 

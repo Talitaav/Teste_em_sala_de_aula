e3b68db20b7c2d020424e7930224a522a3933038
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-03-25 20:10:54 -0300

    Update ExemploContinue.java

--- ExemploContinue.java
+++ ExemploContinue.java
@@ -3,7 +3,7 @@ package exemplo;
 public class ExemploContinue {
 	public static void main(String[] args) {
 		for(int i = 0; i < 10; i++) {
-			if(i ==7) {
+			if(i == 7) {
 				continue;
 			}
 			System.out.println(i);

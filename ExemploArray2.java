65471cac13d5e098678d298bdda400f4e6af8380
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-03-25 21:08:05 -0300

    Add files via upload

---
+++ ExemploArray2.java
@@ -0,0 +1,10 @@
+package exemplo;
+
+public class Exemplo2 {
+	public static void main(String[] args) {
+		String[] cars = { "Volvo", "Bmw", "Ford", "Mazd" };
+		cars[2] = "Open2";
+		System.out.println(cars[0]);
+	}
+
+}

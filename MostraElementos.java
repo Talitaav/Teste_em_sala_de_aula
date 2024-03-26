03c86ade1d633594c7d4874cbd8d114fca736658
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-03-26 19:37:47 -0300

    Add files via upload

---
+++ MostraElementos.java
@@ -0,0 +1,14 @@
+package exemplo;
+
+public class MostraElementos {
+
+	public static void main(String[] args) {
+		String[] carros = { "Chevrolat", "Fiat", "Jaguar", "Ferrari" };
+
+		for (int i = 0; i < carros.length; i++) {
+			System.out.println(carros[i]);
+		}
+
+	}
+
+}

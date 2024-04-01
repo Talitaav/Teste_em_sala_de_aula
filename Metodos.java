57fc194e32c7f2778f16e32903e63dcc24838ad1
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-04-01 20:01:07 -0300

    Add files via upload

---
+++ Metodos.java
@@ -0,0 +1,12 @@
+package exemplo;
+
+public class Metodos {
+	static void primeiroMetodo() {
+		System.out.println("Teste");
+	}
+	public static void main(String[] args) {
+		
+		primeiroMetodo();
+	}
+
+}

46412e6e502601e5b13266d5a02ae81575b07851
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-04-08 21:08:40 -0300

    Add files via upload

---
+++ ExemploClasseObjeto01.java
@@ -0,0 +1,13 @@
+package exemplo;
+
+public class ExemploClasseObjeto01 {
+	int x = 5;
+
+	public static void main(String[] args) {
+
+		ExemploClasseObjeto01 myOBj = new ExemploClasseObjeto01();
+		System.out.println(myOBj.x);
+
+	}
+
+}
